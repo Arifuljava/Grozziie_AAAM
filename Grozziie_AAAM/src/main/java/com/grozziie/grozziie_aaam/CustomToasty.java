b369e6f14e5c53f9c17284d55bf80768016bed36
Arifuljava <arifulislam.java.c@gmail.com>
2023-03-28 11:50:42 +0600

    ui

--- Grozziie_AAAM/src/main/java/com/grozziie/grozziie_aaam/CustomToasty.java
+++ Grozziie_AAAM/src/main/java/com/grozziie/grozziie_aaam/CustomToasty.java
@@ -1,4 +1,10 @@
 package com.grozziie.grozziie_aaam;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class CustomToasty {
+    public  void MyToasty(Context context,String messge) {
+        Toast.makeText(context, ""+messge, Toast.LENGTH_SHORT).show();
+    }
 }
